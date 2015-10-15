8e08b39417939553cb9cfa9c71b1e7aec0454cd4
mikepitre <michaelepitre@gmail.com>
2015-10-15 14:12:55 -0500

    added tapping page UI elements, also created outlets and connected them

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapstxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
 
 
 }
